b49cf9f792fe2bb872554a5f9c47885a7fd91179
Ty Hopp <hopp.ty.c@gmail.com>
2023-05-24 14:57:12 +0800

    Introduce design-system package

---
+++ packages/design-system/src/example/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function Example(): React.ReactElement {
+  const helloWorld = 'Hello World';
+
+  return (
+    <div>{helloWorld}</div>
+  );
+}
+
+export default Example;
